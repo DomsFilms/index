1df11e2bc8f530f85530a174fd8d8054abcb51f1
DomsFilms <147154761+DomsFilms@users.noreply.github.com>
2023-10-06 12:03:42 +0100

    Initial code commit

---
+++ script1.js
@@ -0,0 +1,12 @@
+$(document).ready(() => {
+    $("#id-theme-toggle").on("click", () => {
+        let body = $("body");
+        if (body.hasClass("class-theme-light")) {
+            $("body").removeClass("class-theme-light").addClass("class-theme-dark");
+            $("#id-theme-toggle").html("🌔 light mode");
+        } else {
+            $("body").removeClass("class-theme-dark").addClass("class-theme-light");
+            $("#id-theme-toggle").html("🌘 dark mode");
+        }
+    });
+});
