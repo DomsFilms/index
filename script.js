27b33b4bdceefc8f6aacb915cbd4fa772cf633c7
DomsFilms <147154761+DomsFilms@users.noreply.github.com>
2023-10-06 23:22:47 +0100

    Remember theme choice

--- script.js
+++ script.js
@@ -2,15 +2,29 @@ $(document).ready(() => {
 
     let defaultList = "2023 spooky month";
 
+    setTheme = (theme) => {
+        localStorage.setItem("theme", theme);
+        switch (theme) {
+            case "dark":
+                $("body").removeClass("class-theme-light").addClass("class-theme-dark");
+                $("#id-theme-toggle").html("🌔 light mode");
+                break;
+            case "light":
+            default:
+                $("body").removeClass("class-theme-dark").addClass("class-theme-light");
+                $("#id-theme-toggle").html("🌘 dark mode");
+        }
+    };
+
+    setTheme(localStorage.getItem("theme") || "light");
+
     // Set up the theme toggle button.
     $("#id-theme-toggle").on("click", () => {
         let body = $("body");
         if (body.hasClass("class-theme-light")) {
-            $("body").removeClass("class-theme-light").addClass("class-theme-dark");
-            $("#id-theme-toggle").html("🌔 light mode");
+            setTheme("dark");
         } else {
-            $("body").removeClass("class-theme-dark").addClass("class-theme-light");
-            $("#id-theme-toggle").html("🌘 dark mode");
+            setTheme("light");
         }
     });
 
@@ -22,27 +36,27 @@ $(document).ready(() => {
 
             // Add empty content.
             $("body")
-            .append(
-                $("<div>")
-                .addClass("class-film-card class-film-unwatched class-shadow-small")
-                .attr("id", `id-film-${film}`)
-                .append($("<div>")
-                    .addClass("class-film-bar")
-                    .append(
-                        $("<div>")
-                        .addClass("class-film-title")
-                        .html(film)
-                    )
-                    .append(
-                        $("<div>")
-                        .addClass("class-rating")
-                    )
-                )
                 .append(
                     $("<div>")
-                    .addClass("class-film-review")
-                )
-            );
+                        .addClass("class-film-card class-film-unwatched class-shadow-small")
+                        .attr("id", `id-film-${film}`)
+                        .append($("<div>")
+                            .addClass("class-film-bar")
+                            .append(
+                                $("<div>")
+                                    .addClass("class-film-title")
+                                    .html(film)
+                            )
+                            .append(
+                                $("<div>")
+                                    .addClass("class-rating")
+                            )
+                        )
+                        .append(
+                            $("<div>")
+                                .addClass("class-film-review")
+                        )
+                );
 
             // Load film data.
             let x = new XMLHttpRequest();
@@ -55,55 +69,55 @@ $(document).ready(() => {
                     let card = $(`#id-film-${film}`);
 
                     card.removeClass("class-film-unwatched")
-                    .append(
-                        $("<div>")
-                        .addClass("class-film-summary")
-                        .html(`released: ${x.response.year}, watched: ${x.response.date} ${x.response.seen ? "(seen before)" : "(first time)"}`)
-                    )
-                    .append($("<div>")
-                        .addClass("class-film-bar")
                         .append(
                             $("<div>")
-                            .addClass("class-film-word")
-                            .html(x.response.word.toLowerCase())
+                                .addClass("class-film-summary")
+                                .html(`released: ${x.response.year}, watched: ${x.response.date} ${x.response.seen ? "(seen before)" : "(first time)"}`)
                         )
-                    );
-                    
+                        .append($("<div>")
+                            .addClass("class-film-bar")
+                            .append(
+                                $("<div>")
+                                    .addClass("class-film-word")
+                                    .html(x.response.word.toLowerCase())
+                            )
+                        );
+
                     card.find(".class-film-title")
-                    .html(x.response.title);
+                        .html(x.response.title);
 
                     card.find(".class-rating")
-                    .html(x.response.rating)
-                    .addClass(`class-rating-${x.response.rating}`);
+                        .html(x.response.rating)
+                        .addClass(`class-rating-${x.response.rating}`);
 
                     card.find(".class-film-review")
-                    .html(x.response.review);
+                        .html(x.response.review);
 
                     if (x.response.grotesque !== null) {
                         card.find(".class-film-word")
-                        .after(
-                            $("<div>")
-                            .addClass(`class-rating class-rating-small class-rating-${x.response.grotesque}`)
-                            .html(`grotesque: ${x.response.grotesque}`)
-                        )
+                            .after(
+                                $("<div>")
+                                    .addClass(`class-rating class-rating-small class-rating-${x.response.grotesque}`)
+                                    .html(`grotesque: ${x.response.grotesque}`)
+                            )
                     }
 
                     if (x.response.shock !== null) {
                         card.find(".class-film-word")
-                        .after(
-                            $("<div>")
-                            .addClass(`class-rating class-rating-small class-rating-${x.response.shock}`)
-                            .html(`shock: ${x.response.shock}`)
-                        )
+                            .after(
+                                $("<div>")
+                                    .addClass(`class-rating class-rating-small class-rating-${x.response.shock}`)
+                                    .html(`shock: ${x.response.shock}`)
+                            )
                     }
 
                     if (x.response.suspense !== null) {
                         card.find(".class-film-word")
-                        .after(
-                            $("<div>")
-                            .addClass(`class-rating class-rating-small class-rating-${x.response.suspense}`)
-                            .html(`suspense: ${x.response.suspense}`)
-                        )
+                            .after(
+                                $("<div>")
+                                    .addClass(`class-rating class-rating-small class-rating-${x.response.suspense}`)
+                                    .html(`suspense: ${x.response.suspense}`)
+                            )
                     }
                 }
             };
@@ -127,20 +141,20 @@ $(document).ready(() => {
                 } else {
                     $("body").append(
                         $("<div>")
-                        .attr("id", "id-lists-popup")
-                        .addClass("class-popup class-shadow-large")
+                            .attr("id", "id-lists-popup")
+                            .addClass("class-popup class-shadow-large")
                     );
 
                     Object.keys(c.response).forEach((list, index) => {
                         $("#id-lists-popup")
-                        .append(
-                            $("<button>")
-                            .addclass("class-shadow-small")
-                            .on("click", () => {
-                                populate(c.response[list]);
-                            })
-                            .html(list)
-                        );
+                            .append(
+                                $("<button>")
+                                    .addClass("class-shadow-small")
+                                    .on("click", () => {
+                                        populate(c.response[list]);
+                                    })
+                                    .html(list)
+                            );
                     });
                 }
             });
