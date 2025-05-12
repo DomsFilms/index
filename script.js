1dd9eef52915a40759f6c08456cc9a09a9677446
DomsFilms <147154761+DomsFilms@users.noreply.github.com>
2025-05-12 16:48:07 +0100

    Add Things Will Be Different and The Wind

--- script.js
+++ script.js
@@ -580,9 +580,9 @@ $(document).ready(() => {
 
 		// Only consider films rated 7 and up.
 		// Only consider films watched before the start of the previous week, or the recommendation will change mid-week when new reviews are added.
-		// 604800000 is one week in milliseconds. This gives me a window to review recently watched films, but not too long to prevent laziness.
+		// 1360800000 is two weeks in milliseconds. This gives me a window to review recently watched films, but not too long to prevent laziness.
 		const films = sortDate(catalogueFilms
-			.filter(film => film.rating >= 7 && parseDate(film.date) < date - 604800000),
+			.filter(film => film.rating >= 7 && parseDate(film.date) < 1745708400),//date - 1360800000),
 			false);
 
 		// The date has the ms value set to 0, to be consistent, but this makes the total time in ms end in loads of 0s.
